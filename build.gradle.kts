906d086cb7c063b75beeb33a3be868f8f4e6b4e8
Vasili Karaev <kratos661@gmail.com>
2020-09-13 03:33:00 +0300

    add project specific gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.4.0-SNAPSHOT"
+    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+
+    kotlin("jvm") version "1.4.0"
+    kotlin("plugin.jpa") version "1.4.0"
+    kotlin("plugin.spring") version "1.4.0"
+}
+
+group = "com.github.toncherami.mpd"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/milestone") }
+    maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
+    implementation("org.springframework.boot:spring-boot-starter-integration")
+
+    implementation("org.springframework.integration:spring-integration-ip")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+//	implementation("org.liquibase:liquibase-core")
+
+    runtimeOnly("org.postgresql:postgresql")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "11"
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+    }
+}
