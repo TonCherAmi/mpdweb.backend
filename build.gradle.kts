69a7f488c8dc4da8902634f3996fea1b4b25d92e
Vasili Karaev <kratos661@gmail.com>
2021-11-01 23:12:18 +0300

    up spring version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.4.0-SNAPSHOT"
-    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    id("org.springframework.boot") version "2.4.0"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
 
     kotlin("jvm") version "1.4.0"
     kotlin("plugin.jpa") version "1.4.0"
