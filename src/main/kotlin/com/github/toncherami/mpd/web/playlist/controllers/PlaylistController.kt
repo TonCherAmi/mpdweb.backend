3976a14711688d70b3fe53e07ec9fcc660ef556f
Vasili Karaev <kratos661@gmail.com>
2022-05-18 02:47:59 +0300

    change patch -> post

--- src/main/kotlin/com/github/toncherami/mpd/web/playlist/controllers/PlaylistController.kt
+++ src/main/kotlin/com/github/toncherami/mpd/web/playlist/controllers/PlaylistController.kt
@@ -7,7 +7,7 @@ import com.github.toncherami.mpd.web.playlist.dto.api.request.PlaylistReplaceBod
 import com.github.toncherami.mpd.web.playlist.services.PlaylistService
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -22,7 +22,7 @@ class PlaylistController(private val playlistService: PlaylistService) {
         return playlistService.get()
     }
 
-    @PatchMapping
+    @PostMapping
     fun add(@RequestBody body: PlaylistAddBody) {
         playlistService.add(body.uri)
     }
